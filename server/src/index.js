1540c88f9a20cf35ad4d190833345110bc54c2a0
AlaaAbbas22 <alaa@uni.minerva.edu>
2025-10-08 17:29:43 -0700

    add 0.0.0.0

--- server/src/index.js
+++ server/src/index.js
@@ -92,7 +92,7 @@ const cleanupSessions = async () => {
 // cleanupSessions();
 
 // Start Server
-server.listen(PORT, (err) => {
+server.listen(PORT, "0.0.0.0", (err) => {
   if (err) console.log(err);
   console.log("Server running on Port", PORT);
 });
