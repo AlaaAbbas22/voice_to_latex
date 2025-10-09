abeaed58610abe4a1f237bbe93561a13088dde93
AlaaAbbas22 <alaa@uni.minerva.edu>
2025-10-08 21:39:36 -0700

    add sameSite and secure attr

--- server/src/index.js
+++ server/src/index.js
@@ -58,6 +58,8 @@ const sessionMiddleware = session({
   cookie: {
     maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days in milliseconds
     httpOnly: false,
+    sameSite: process.env.ENVIRONMENT !== "DEV" ? "none" : undefined,
+    secure: process.env.ENVIRONMENT !== "DEV" ? true : undefined,
   },
 });
 
